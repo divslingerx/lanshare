dad9a00404a2aa0474ffaaaa17b7b3405f456114
Taylor O'Reilly <taylor@taylororeilly.dev>
2026-04-29 11:52:32 -0400

    chore: regenerate models.ts with int64 last_synced_at

--- frontend/wailsjs/go/models.ts
+++ frontend/wailsjs/go/models.ts
@@ -5,8 +5,7 @@ export namespace config {
 	    remote_folder: string;
 	    folder_id: string;
 	    local_dest: string;
-	    // Go type: time
-	    last_synced_at: any;
+	    last_synced_at: number;
 	
 	    static createFrom(source: any = {}) {
 	        return new Subscription(source);
@@ -18,26 +17,8 @@ export namespace config {
 	        this.remote_folder = source["remote_folder"];
 	        this.folder_id = source["folder_id"];
 	        this.local_dest = source["local_dest"];
-	        this.last_synced_at = this.convertValues(source["last_synced_at"], null);
+	        this.last_synced_at = source["last_synced_at"];
 	    }
-	
-		convertValues(a: any, classs: any, asMap: boolean = false): any {
-		    if (!a) {
-		        return a;
-		    }
-		    if (a.slice && a.map) {
-		        return (a as any[]).map(elem => this.convertValues(elem, classs));
-		    } else if ("object" === typeof a) {
-		        if (asMap) {
-		            for (const key of Object.keys(a)) {
-		                a[key] = new classs(a[key]);
-		            }
-		            return a;
-		        }
-		        return new classs(a);
-		    }
-		    return a;
-		}
 	}
 	export class Folder {
 	    id: string;
